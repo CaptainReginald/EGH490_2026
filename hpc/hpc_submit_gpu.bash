004be974a82ff917a68cea765105c4d544c05d61
Java Terry <33javalava@gmail.com>
2026-02-03 09:31:35 +1000

    2025 Config scripts for genera model

--- hpc/hpc_submit_gpu.bash
+++ hpc/hpc_submit_gpu.bash
@@ -2,9 +2,8 @@
 
 #PBS -N dataset_analysis
 #PBS -l walltime=12:00:00
-#PBS -l walltime=12:00:00
-#PBS -l ncpus=8
-#PBS -l mem=32gb
+#PBS -l ncpus=12
+#PBS -l mem=64gb
 #PBS -l ngpus=1
 #PBS -l gpu_id=A100
 #PBS -m abe
